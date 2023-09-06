7d5c6e2230287d90438d3d498abdc46383b60151
tlc2880 <tommylcao@gmail.com>
2023-09-05 20:10:50 -0700

    Implement new component StringArray and push new input word

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Counter from "./Counter";
-import CounterArray from "./CounterArray";
+import NumberArray from "./NumberArray";
+import StringArray from "./StringArray";
 
 function App() {
 
   return (
     <>
       <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
-      <CounterArray>{(numArray: number[]) => <>Num Array: {JSON.stringify(numArray)}</>}</CounterArray>
+      <NumberArray>{(numArray: number[]) => <>Num Array: {JSON.stringify(numArray)}</>}</NumberArray>
+      <StringArray>{(strArray: string[]) => <>String Array: {JSON.stringify(strArray)}</>}</StringArray>
     </>
   )
 }
